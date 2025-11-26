2a78ba4db7646fd34502b84944513e8f82237da3
Moretty <142655155+ivan78789@users.noreply.github.com>
2025-11-26 12:58:57 +0600

    маленькие добавления

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+        <title>{{ config('app.name', 'GetGrant,Dachboard') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
 </head>
